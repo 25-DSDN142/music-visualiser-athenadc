b4655ed3b56a09793e4d315fbdf48c55c286c646
athenadc <athenadelacruzart@gmail.com>
2025-09-14 04:30:17 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,21 +1,18 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
 let firstRun = true;
-let myImage;
-let backgroundImage;
+let myImage; //character
 
-// let leftScreen = (109, 140, 244);
-// let rightScreen = (190, 34, 65);
-let squareXmove = 0; //blue background square animation X
-let squareYmove = 100 // blue background square animation Y
+let squareXmove = 0; //moving square animation X
+let squareYmove = 100; // moving square animation Y
 let squareSize = 50; //grid square size
 let squareXpos = 20; //grid square X
 let squareYpos = 50; //grid square Y
 
 let r = 0; //pills rotate
 
-let startCol;
-let endCol;
+let startCol; //lerpColor
+let endCol; //lerpColor
 
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
@@ -27,25 +24,25 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       squareSize = map(bass, 0, 100, 20, 80);
       squareDrum = map(drum, 0, 100, 5, 60);
       squareVocal = map(vocal, 0, 100, 0, 50);
-      pillBubble = map(words, 0, 100, 5, 20);
-      backSquareCol = map(0, 255, 0, 1200)
-      startCol = color(0, 150, 195)
-      endCol = color(190, 34, 65)
+      pillBubble = map(words, 0, 100, 10, 30);
+      startCol = color(0, 150, 195); //moving squares starts blue
+      endCol = color(190, 34, 65); //moving squares ends red
    }
 
-let lerpAmount = map(squareXmove, 0, height, 0, 1);
-let interCol = lerpColor(startCol, endCol, lerpAmount);
+   //Moving squares gradient
+   let lerpAmount = map(squareXmove, 0, height, 0, 1);
+   let interCol = lerpColor(startCol, endCol, lerpAmount);
 
-{  
-//Moving blue squares 1
-   rectMode(CENTER);
-   fill(interCol);
-   square(10 + squareXmove, squareYmove, squareSize, 10);
-   square(40 + squareXmove, 190 + squareYmove, squareSize, 10);
-   square(10 + squareXmove, 390 + squareYmove, squareSize, 10);
-   square(40 + squareXmove, 590 + squareYmove, squareSize, 10);
+   {
+      //Moving squares 1
+      rectMode(CENTER);
+      fill(interCol);
+      square(10 + squareXmove, squareYmove, squareSize, 10);
+      square(40 + squareXmove, 190 + squareYmove, squareSize, 10);
+      square(10 + squareXmove, 390 + squareYmove, squareSize, 10);
+      square(40 + squareXmove, 590 + squareYmove, squareSize, 10);
 
-   //Moving blue squares 2
+      //Moving squares 2
       rectMode(CENTER);
       stroke(255);
       strokeWeight(2);
@@ -75,6 +72,7 @@ let interCol = lerpColor(startCol, endCol, lerpAmount);
             square(squareXpos, squareYpos + (l * 160), squareVocal, 5);
          }
 
+
          //Character Image
       }
       if (firstRun) {
@@ -83,6 +81,7 @@ let interCol = lerpColor(startCol, endCol, lerpAmount);
 
          firstRun = false;
       }
+
       image(myImage, 0, 0);
 
       //to restart background square animation
@@ -92,31 +91,11 @@ let interCol = lerpColor(startCol, endCol, lerpAmount);
       }
    }
 
-   // //Speech bubble
-   // {
-   //    push()
-   //    fill(200);
-   //    stroke(1);
-   //    rect(600, 180, 500, speechBubbleSize, 20);
-
-   // }
-   // if (speechBubbleSize > 200) {
-   //    grow = false;
-   // }
-   // if (grow == true) {
-   //    speechBubbleSize += growAmount;
-   // } else {
-   //    speechBubbleSize == growAmount;
-   // }
-   // pop()
-
-   //Text Bubble
-
    if (words) {
       push()
-      translate(600, 250)
+      translate(600, 250);
       beginShape();
-      fill(255)
+      fill(255);
       vertex(-105, -86);
       bezierVertex(-200, 140, -605, -160, 100, -160);
       bezierVertex(73, -160, 500, -165, 250, -160);
@@ -134,30 +113,16 @@ let interCol = lerpColor(startCol, endCol, lerpAmount);
    textFont('Courier New', 50);
    text(words, 420, 200);
 
-   //  push()
-   // if (songIsPlaying) {
-   //    fill(200);
-   //    translate(width/2, height/2);
-   //    rect(CENTER)
-   //    rotate(angle);
-   //    ellipse(0 + squareXmove, 0, 80, pillBubble);
-   //    ellipse(100 + squareXmove, 100, 80, pillBubble );
-   //    ellipse(200 + squareXmove, 200, 80, pillBubble);
-   //    pop()
-   // }
-
-   // angle -= radians(80);
-
-   //pills
+   //spinning pills
    translate(950, 360);
 
    for (let i = 0; i < 10; i++) {
       rotate(r);
-      fill(255, 157, 92)
+      fill(255, 157, 92);
       ellipse(i, 220, 30, 60);
    }
 
-
+   //trigger for spinning pills
    if (songIsPlaying) {
       r = r + 0.2;
    }
